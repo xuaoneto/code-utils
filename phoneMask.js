aa9704ec3e5b7558a1e5ac93dc8a760656ecf207
João Fernandes Praxedes Neto <jofernandes47@gmail.com>
2022-07-16 00:43:50 -0300

    ups

--- phoneMask.js
+++ phoneMask.js
@@ -5,3 +5,14 @@ function maskPhone(value) {
     ? format[1]
     : "(" + format[1] + ") " + format[2] + (format[3] ? "-" + format[3] : "");
 }
+// Typescript
+// function maskPhone(value: string): string {
+//   let format = value.replace(/\D/g, "").match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+//   if (format) {
+//     const formated = !format[2]
+//       ? format[1]
+//       : "(" + format[1] + ") " + format[2] + (format[3] ? "-" + format[3] : "");
+//     return formated;
+//   }
+//   return "";
+// }
